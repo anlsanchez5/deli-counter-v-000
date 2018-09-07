5efc41004dacff5accfa341befca69f6b8cef303
anlsanchez@ucdavis.edu <anlsanchez@ucdavis.edu>
2018-09-07 18:57:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+katz_deli = []
+
+def line(deli_line)
+
+  if deli_line.length == 0
+    "The line is currently empty."
+  else 
+    number = 1
+    current_line = []
+    deli_line.each do |people| 
+      current_line << "#{number}. #{people} "
+      number += 1
+    end
+    "The current line is: " + current_line.join(' ')
+    
+
+end
