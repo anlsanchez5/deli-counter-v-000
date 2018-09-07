328aec5ef2e4759316e0fc8e67cc9ba2bedde953
anlsanchez@ucdavis.edu <anlsanchez@ucdavis.edu>
2018-09-07 18:58:00 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,14 +4,12 @@ def line(deli_line)
 
   if deli_line.length == 0
     "The line is currently empty."
-  else 
+  else
     number = 1
     current_line = []
-    deli_line.each do |people| 
+    deli_line.each do |people|
       current_line << "#{number}. #{people} "
       number += 1
     end
     "The current line is: " + current_line.join(' ')
-    
-
 end
